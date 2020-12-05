6161b9a283bd06abfb164db6f19af9532c1541ee
K-Rintaro <k.rintaro1@icloud.com>
2020-12-05 20:52:48 +0900

    🎉United Square

--- src/unitedsquare.kt
+++ src/unitedsquare.kt
@@ -1,15 +1,14 @@
 import javafx.application.Application
-import javafx.scene.Group
-import javafx.scene.PerspectiveCamera
-import javafx.scene.Scene
+import javafx.scene.*
 import javafx.scene.image.Image
 import javafx.scene.input.MouseEvent
 import javafx.scene.paint.Color
 import javafx.scene.paint.PhongMaterial
 import javafx.scene.shape.Sphere
+import javafx.scene.text.Font
+import javafx.scene.text.Text
 import javafx.stage.Stage
 import javafx.scene.transform.Rotate
-
 
 
 class HunHun : Application() {
@@ -25,22 +24,38 @@ class HunHun : Application() {
         val material = PhongMaterial();
         material.diffuseMap = earthpic;
         theearth.material = material
+        theearth.rotate = 23.4
 
+        val syuumatsu = Text()
+        syuumatsu.text = "23:58.40"
+        syuumatsu.font = Font.font("Helvetica", 8.0)
+        syuumatsu.fill = Color.WHITE
+        syuumatsu.layoutX = -13.0
+        syuumatsu.layoutY = -12.0
 
-        val group = Group(theearth)
+        val group = Group(syuumatsu,theearth)
+
+        val point = PointLight(Color.RED);
+        point.translateX = 240.0
+        point.translateY = 0.0
+        point.translateZ = -250.0
+        group.children.add(point);
+
+        val light1 = AmbientLight(Color.rgb(100, 100, 100, 0.5));
+        group.children.add(light1);
 
         val scene = Scene(group, 640.0, 320.0, Color.BLACK)
 
         val rotateX = Rotate(0.0, Rotate.X_AXIS)
         val rotateY = Rotate(0.0, Rotate.Y_AXIS)
-        group.transforms.addAll(rotateX, rotateY)
+        theearth.transforms.addAll(rotateX, rotateY)
 
-        scene.addEventHandler(MouseEvent.MOUSE_PRESSED) { e ->
+        theearth.addEventHandler(MouseEvent.MOUSE_PRESSED) { e ->
             this.x = e.getSceneX()
             this.y = e.getSceneY()
         }
 
-        scene.addEventHandler(MouseEvent.MOUSE_DRAGGED) { e ->
+        theearth.addEventHandler(MouseEvent.MOUSE_DRAGGED) { e ->
             val nowX: Double = e.getSceneX()
             val nowY: Double = e.getSceneY()
             val dx = x - nowX
